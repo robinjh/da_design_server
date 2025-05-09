908b9bd1cc4aa0c7d170a6a933cab55ce389e1a4
Jaehwan_Kim <robinjh0314@gmail.com>
2025-05-09 15:09:29 +0900

    logger second commit

---
+++ test.py
@@ -0,0 +1,20 @@
+def test_logger():
+    """Test logger.
+
+    :return: test result.
+    :rtype: bool
+    """
+    from da_design_server.src import mylogger
+    try:
+        m = mylogger.get_logger('test', '/home/u1006/da_design_server/log')
+        m.debug('hi, debug')
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+if __name__ == '__main__':
+    ret = test_logger()
+    if not ret:
+        raise Exception('Error when test_logger')
+    print('Success - test_logger')
